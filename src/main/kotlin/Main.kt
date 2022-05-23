6f953d20dee22c312bd9d34fede37149fe8d5205
leshukav <sc-service@bk.ru>
2022-05-23 11:41:04 +0300

    correction of defects

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,30 @@
 fun main() {
- // Задача №1 - «Только что»
-    val time = 156000
-    when (time) {
-        in 0..60 -> println("был(а) только что")
-        in 61..3600 -> println("был(а) ${time / 60} ${minuteAgo(time)}")
-        in 3601..86400 -> println(
-            "был(а) ${time / 3600} ${hourAgo(time)} и ${(time - time / 3600 * 3600) / 60}  ${
-                minuteAgo(
-                    (time - time / 3600 * 3600) / 60
-                )
-            }"
-        )
-        in 86_401..172_800 -> println("был(а) сегодня")
-        in 172_801..259_200 -> println("был(а) вчера")
-        else -> println("был(а) давно")
-    }
-
+    // Задача №1 - «Только что»
+    val time = 16000
+    println(agoToText(time))
 }
 
- fun hourAgo(time: Int): String {
-     return when (time / 3600) {
-         1, 21 -> "час"
-         2, 3, 4, 22, 23, 24 -> "часа"
-         else -> "часов"
-     }
- }                                                                                                                          
+fun agoToText(time: Int): String {
+    return when (time) {
+        in 0..60 -> "был(а) только что"
+        in 61..3600 -> "был(а) ${time / 60} ${minuteAgo(time)}"
+        in 3601..86400 ->
+            "был(а) ${time / 3600} ${hourAgo(time)} и ${(time - time / 3600 * 3600) / 60}  ${
+                minuteAgo((time - time / 3600 * 3600) / 60)
+            }"
+        in 86_401..172_800 -> "был(а) сегодня"
+        in 172_801..259_200 -> "был(а) вчера"
+        else -> "был(а) давно"
+    }
+}
+
+fun hourAgo(time: Int): String {
+    return when (time / 3600) {
+        1, 21 -> "час"
+        2, 3, 4, 22, 23, 24 -> "часа"
+        else -> "часов"
+    }
+}
 
 fun minuteAgo(time: Int): String {
     return when (time / 60) {
